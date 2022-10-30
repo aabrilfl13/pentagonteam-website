b80a05ecd5eec38dd9dd62e0d869b2b0e27e4673
Antonio Abril <antonio.abril@octoenergy.com>
2022-10-30 03:17:14 +0100

    Add Footer id

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,29 +1,7 @@
 import React from "react";
+
 import "./footer.css";
 
 export const Footer = () => {
-	return <div>Footer
-
-		<br />
-		<br />
-		<br />
-		<br />
-		<br />
-		<br />
-		<br />
-		<br />
-		<br />
-		<br />
-		<br />
-		<br />
-		<br />
-		<br />
-		<br />
-		<br />
-		<br />
-		<br />
-		<br />
-		<br />
-		<br />
-	</div>;
+	return <div id="footer">Footer</div>;
 };
