7504e4ee02ebe1184f8af455e3eaf8dda46ec4b6
Antonio Abril <antonio.abril@octoenergy.com>
2022-10-30 11:58:34 +0100

    Add services

--- src/components/services/Services.js
+++ src/components/services/Services.js
@@ -1,6 +1,56 @@
 import React from "react";
+import { BiBadgeCheck } from "react-icons/bi";
+
 import "./services.css";
 
 export const Services = () => {
-	return <div>Services</div>;
+	return (
+		<section id="services">
+			<h2>Servicios</h2>
+			<h5>Que Te Ofrecemos</h5>
+
+			<div className="container container__services">
+				<article className="service">
+					<div className="service__head">
+						<h3>Alquiler de equipo</h3>
+					</div>
+
+					<ul className="service__list">
+						<li>
+							<BiBadgeCheck className="service__list-icon" />
+							<p>Alquiler de réplica</p>
+						</li>
+						<li>
+							<BiBadgeCheck className="service__list-icon" />
+							<p>Máscara</p>
+						</li>
+						<li>
+							<BiBadgeCheck className="service__list-icon" />
+							<p>Gafas de protección</p>
+						</li>
+						<li>
+							<BiBadgeCheck className="service__list-icon" />
+							<p>Munición infinita</p>
+						</li>
+					</ul>
+				</article>
+				<article className="service">
+					<div className="service__head">
+						<h3>Suministros</h3>
+					</div>
+
+					<ul className="service__list">
+						<li>
+							<BiBadgeCheck className="service__list-icon" />
+							<p>Bebida</p>
+						</li>
+						<li>
+							<BiBadgeCheck className="service__list-icon" />
+							<p>Almuerzo</p>
+						</li>
+					</ul>
+				</article>
+			</div>
+		</section>
+	);
 };
