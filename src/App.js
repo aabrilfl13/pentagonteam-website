9390fe35dad9df66844478dcbc377cf5223cdb5e
Antonio Abril <antonio.abril@octoenergy.com>
2022-10-31 10:32:06 +0100

    Fix and Add new team component

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import React from "react";
 import "./App.css";
 import { About } from "./components/about/About";
 import { Contact } from "./components/contact/Contact";
-import { Experience } from "./components/experience/Experience";
 import { Footer } from "./components/footer/Footer";
 import { Header } from "./components/header/Header";
 import { Nav } from "./components/nav/Nav";
 import { Portfolio } from "./components/portfolio/Portfolio";
 import { Services } from "./components/services/Services";
+import { Team } from "./components/team/Team";
 import { Testimonials } from "./components/testimonials/Testimonials";
 
 function App() {
@@ -18,7 +18,7 @@ function App() {
 			<Header />
 			<Nav />
 			<About />
-			<Experience />
+			<Team />
 			<Services />
 			<Portfolio />
 			<Testimonials />
--- src/components/experience/Experience.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import "./experience.css";
-
-export const Experience = () => {
-	return <div>Experience</div>;
-};
