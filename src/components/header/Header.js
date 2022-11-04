a9257aa122be1a7539a2c4d1f93825f80565b399
Antonio Abril <antonio.abril@octoenergy.com>
2022-11-04 20:05:21 +0100

    Add header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,17 +1,22 @@
 import React from "react";
 
+import logoHeader from "../../assets/img/PENTAGON-IMAGOTIPO-PENTAGONO-TRANSPARENCY.png";
+import videoHeader from "../../assets/img/portada.mp4";
 import SocialMedia from "../floating/SocialMedia";
 import "./header.css";
 
 export const Header = () => {
 	return (
-		<div>
-			<header id="header">
-				<div className="container header__container">
-					<h1>Aquí va la portada bro</h1>
-					<SocialMedia />
+		<header id="header">
+			<div className="container container__header">
+				<div className="image__header">
+					<img src={logoHeader} alt="logo" />
 				</div>
-			</header>
-		</div>
+				<video autoPlay loop muted>
+					<source src={videoHeader} type="video/mp4" />
+				</video>
+				{/* <SocialMedia /> */}
+			</div>
+		</header>
 	);
 };
